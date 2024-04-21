a8a1a1d1adc69294050ca322c65c4f5fb4d4a130
CarlaSablic95 <carlasablic95@gmail.com>
2024-04-21 19:24:57 -0300

    Cambios de estilos en el cuento de Simon

--- src/assets/pages/cuentos/CuentoSimon.jsx
+++ src/assets/pages/cuentos/CuentoSimon.jsx
@@ -9,32 +9,26 @@ const CuentoSimon = () => {
         <>
         {/* CUENTO */}
             <section className="container py-4">
-                <div className="row justify-content-evenly mt-5 pt-5">
-                    <div className="col-10 col-md-4 text-center">
-                        <img src={ Pintor } alt="Imagen de un niño pintor" className="rounded-5 mb-5 shadow-sm img-fluid" />
-                    </div>
-                    <div className="col-10 col-md-6">
-                            <div className="">
-                        <h1 className="mb-4 fs-2">Simon, un pequeño gran artista</h1>
-                                <p>Había una vez en un pequeño pueblo de Francia, un niño llamado Simon, conocido por todos por su gran pasión: la pintura. Con su gran creatividad y su pincel en mano, Simon creaba mundos de colores que dejaban a todos maravillados.</p>
-                                <p>Un día, se anunció una gran competencia de pintura en el pueblo. Los mejores artistas de todas partes llegarían con años de experiencia y obras magníficas. Simon, con solo diez años, era el participante más joven.</p>
-                                <p>“Son demasiado buenos, y yo solo soy un niño sin experiencia”, pensaba Simon, mirando su lienzo en blanco. Pero sus padres, viendo su tristeza, se acercaron y le dijeron: “Simon, no importa la edad o la experiencia. Dios te ha dado un talento único que puede tocar el corazón de las personas. No tenés que ganar para ser especial; ya lo sos porque Dios te creó con gran amor y con un gran propósito.”</p>
-                            </div>
-                            <div className="">
-                                <p>Inspirado por las palabras de sus padres, Simon tomó su pincel con nueva determinación. Pintó como nunca antes, con cada trazo lleno de la alegría y la esperanza que sentía en su corazón.</p>
-                                <p>El día de la competencia llegó, y los pintores presentaron sus obras. Había paisajes realistas, retratos detallados y escenas que parecían cobrar vida. Cuando llegó el turno de Simon, todos se quedaron en silencio. Su pintura era diferente: un paisaje sencillo pero lleno de vida, con colores que bailaban en el lienzo y transmitían una felicidad pura.</p>
-                                <p>Los jueces quedaron impresionados. “Esta obra nos recuerda la belleza de ver el mundo a través de los ojos de un niño,” dijeron. Y así, contra todo pronóstico, Simon ganó el premio mayor.</p>
-                            </div>
-                            <div className="">
-                                <p>
+                <div className="mt-5 pt-5">
+                        <div className="text-center mb-5">
+                            <img src={ Pintor } alt="Imagen de un niño pintor" className="rounded-5 shadow-sm" style={{ width: "450px" }}  />
+                        </div>
+                            <div className="container text-center text-md-start">
+                                <h1 className="mb-4 fs-2 text-center">Simon, un pequeño gran artista</h1>
+                                <p style={{ fontSize:"1.1rem" }}>Había una vez en un pequeño pueblo de Francia, un niño llamado Simon, conocido por todos por su gran pasión: la pintura. Con su gran creatividad y su pincel en mano, Simon creaba mundos de colores que dejaban a todos maravillados.</p>
+                                <p style={{ fontSize:"1.1rem" }}>Un día, se anunció una gran competencia de pintura en el pueblo. Los mejores artistas de todas partes llegarían con años de experiencia y obras magníficas. Simon, con solo diez años, era el participante más joven.</p>
+                                <p style={{ fontSize:"1.1rem" }}>“Son demasiado buenos, y yo solo soy un niño sin experiencia”, pensaba Simon, mirando su lienzo en blanco. Pero sus padres, viendo su tristeza, se acercaron y le dijeron: “Simon, no importa la edad o la experiencia. Dios te ha dado un talento único que puede tocar el corazón de las personas. No tenés que ganar para ser especial; ya lo sos porque Dios te creó con gran amor y con un gran propósito.”</p>
+                            
+                                <p style={{ fontSize:"1.1rem" }}>Inspirado por las palabras de sus padres, Simon tomó su pincel con nueva determinación. Pintó como nunca antes, con cada trazo lleno de la alegría y la esperanza que sentía en su corazón.</p>
+                                <p style={{ fontSize:"1.1rem" }}>El día de la competencia llegó, y los pintores presentaron sus obras. Había paisajes realistas, retratos detallados y escenas que parecían cobrar vida. Cuando llegó el turno de Simon, todos se quedaron en silencio. Su pintura era diferente: un paisaje sencillo pero lleno de vida, con colores que bailaban en el lienzo y transmitían una felicidad pura.</p>
+                                <p style={{ fontSize:"1.1rem" }}>Los jueces quedaron impresionados. “Esta obra nos recuerda la belleza de ver el mundo a través de los ojos de un niño,” dijeron. Y así, contra todo pronóstico, Simon ganó el premio mayor.</p>
+                            
+                                <p style={{ fontSize:"1.1rem" }}>
                                 Cuando Simon subió al escenario para recibir su premio, les dijo a todos: “Al principio, pensaba que no tenía oportunidad, pero aprendí algo importante: todos somos especiales y únicos. Cada uno de nosotros tiene algo maravilloso que ofrecer al mundo”.
                                 </p>
-                                <p>Con esas palabras, Simon no solo se llevó el premio a casa, sino que también dejó una lección valiosa en el corazón de cada persona que lo escuchó. Y desde ese día, todos en el pueblo empezaron a ver sus propias habilidades con nuevos ojos, apreciando la singularidad que Dios había puesto en ellos.</p>
+                                <p style={{ fontSize:"1.1rem" }}>Con esas palabras, Simon no solo se llevó el premio a casa, sino que también dejó una lección valiosa en el corazón de cada persona que lo escuchó. Y desde ese día, todos en el pueblo empezaron a ver sus propias habilidades con nuevos ojos, apreciando la singularidad que Dios había puesto en ellos.</p>
                             </div>
                         </div>
-                    
-                        
-                </div>
             </section>
 
             {/* ACTIVIDADES */}
@@ -46,15 +40,15 @@ const CuentoSimon = () => {
                         alt="ícono de lápiz"
                         style={{ width: "35px", transform: "rotate(-20deg)" }}
                     /></h2>
-                    <p className="text-center">A partir del cuento que leíste, copiá y respondé en tu cuaderno, las siguientes preguntas:</p>
+                    <p>A partir del cuento que leíste, copiá y respondé en tu cuaderno, las siguientes preguntas:</p>
                     <div className="d-flex align-items-center">
                         
                     <div>
-                        <ol>
-                            <li>¿Cuál es la pasión de Simon?</li>
-                            <li>¿Por qué motivo Simon se sentía triste?</li>
-                            <li>¿Qué consejo le dieron sus padres cuando vieron triste a Simon?</li>
-                            <li>¿Qué dijeron los jueces al ver la obra de pintura creada por Simon?</li>
+                        <ol className="ps-3">
+                            <li style={{ fontSize:"1.1rem" }}>¿Cuál es la pasión de Simon?</li>
+                            <li style={{ fontSize:"1.1rem" }}>¿Por qué motivo Simon se sentía triste?</li>
+                            <li style={{ fontSize:"1.1rem" }}>¿Qué consejo le dieron sus padres cuando vieron triste a Simon?</li>
+                            <li style={{ fontSize:"1.1rem" }}>¿Qué dijeron los jueces al ver la obra de pintura creada por Simon?</li>
                         </ol>
                     </div>
                 </div>
@@ -85,20 +79,20 @@ const CuentoSimon = () => {
                 
                 {/* REFLEXIÓN */}
                 <div className="mb-4">
-                    <h3 className="text-center">Para reflexionar.. <img
+                    <h3 className="text-center">Para reflexionar... <img
                         src={ Nube }
                         alt="ícono de nube"
                         style={{ width: "35px", transform: "rotate(5deg)" }}
                     /></h3>
-                    <p className="text-center">Basándote en la historia de Simon, tomá un momento para pensar y respondé estas preguntas en tu cuaderno:</p>
+                    <p>Basándote en la historia de Simon, tomá un momento para pensar y respondé estas preguntas en tu cuaderno:</p>
                     <div className="d-flex align-items-center">
                         
                     <div>
-                        <ul>
-                            <li>¿Cuál es tu mayor pasión?</li>
-                            <li>¿Alguna vez te sentiste triste como Simon? Si tu respuesta es “Sí”, ¿qué hiciste para superarlo?</li>
-                            <li>¿Qué consejo le darías a un amigo que se siente triste por no creer en su propio talento?</li>
-                            <li>Habla con tus papás y amigos sobre lo increíble que es saber que Dios nos hizo a todos con un propósito especial. Él nos dio habilidades súper especiales para hacer cosas geniales en el mundo. ¿Sabes cuál es tu habilidad especial?</li>
+                        <ul className="ps-3">
+                            <li style={{ fontSize:"1.1rem" }}>¿Cuál es tu mayor pasión?</li>
+                            <li style={{ fontSize:"1.1rem" }}>¿Alguna vez te sentiste triste como Simon? Si tu respuesta es “Sí”, ¿qué hiciste para superarlo?</li>
+                            <li style={{ fontSize:"1.1rem" }}>¿Qué consejo le darías a un amigo que se siente triste por no creer en su propio talento?</li>
+                            <li style={{ fontSize:"1.1rem" }}>Habla con tus papás y amigos sobre lo increíble que es saber que Dios nos hizo a todos con un propósito especial. Él nos dio habilidades súper especiales para hacer cosas geniales en el mundo. ¿Sabes cuál es tu habilidad especial?</li>
                         </ul>
                     </div>
                 </div>
